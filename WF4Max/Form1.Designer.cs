e59795832e6566b66ca72a23b4a759548f583702
WendyAndAndy <184327932@qq.com>
2018-08-29 21:21:43 +0800

    0009 用VisualStudio设计Max工具GUI
    0009 用VisualStudio设计Max工具GUI

---
+++ WF4Max/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WF4Max
+{
+    partial class MyWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyWindow));
+            this.btnBox = new System.Windows.Forms.Button();
+            this.btnTeapot = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.txtStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBox
+            // 
+            this.btnBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnBox.Location = new System.Drawing.Point(11, 20);
+            this.btnBox.Name = "btnBox";
+            this.btnBox.Size = new System.Drawing.Size(114, 42);
+            this.btnBox.TabIndex = 0;
+            this.btnBox.Text = "create Box";
+            this.btnBox.UseVisualStyleBackColor = true;
+            this.btnBox.Click += new System.EventHandler(this.btnBox_Click);
+            // 
+            // btnTeapot
+            // 
+            this.btnTeapot.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnTeapot.Location = new System.Drawing.Point(134, 20);
+            this.btnTeapot.Name = "btnTeapot";
+            this.btnTeapot.Size = new System.Drawing.Size(114, 42);
+            this.btnTeapot.TabIndex = 1;
+            this.btnTeapot.Text = "create Teapot";
+            this.btnTeapot.UseVisualStyleBackColor = true;
+            this.btnTeapot.Click += new System.EventHandler(this.btnTeapot_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnTeapot);
+            this.groupBox1.Controls.Add(this.btnBox);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 77);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(262, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 379);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(44, 17);
+            this.txtStatus.Text = "Ready";
+            // 
+            // MyWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 401);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MyWindow";
+            this.ShowInTaskbar = false;
+            this.Text = "WendyAndAndy";
+            this.TopMost = true;
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.ToolStripStatusLabel txtStatus;
+        public System.Windows.Forms.Button btnBox;
+        public System.Windows.Forms.Button btnTeapot;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ WF4Max/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WF4Max
+{
+    public partial class MyWindow : Form
+    {
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ShowStatus(object sender)
+        {
+            this.txtStatus.Text = (sender as Button).Text;
+        }
+
+        private void btnBox_Click(object sender, EventArgs e)
+        {
+            ShowStatus(sender);
+        }
+
+        private void btnTeapot_Click(object sender, EventArgs e)
+        {
+            ShowStatus(sender);
+        }
+    }
+}
